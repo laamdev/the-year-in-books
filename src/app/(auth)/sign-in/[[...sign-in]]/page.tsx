34b329e839ef7221a75af6ba9dc5e9c1dd429f9b
Luis Anaya <brothersailorwebdev@gmail.com>
2024-01-30 20:21:22 +0100

    style auth cards

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -9,17 +9,29 @@ export const metadata: Metadata = {
 export default function SignInPage() {
   return (
     <SignIn
-      signUpUrl="/sing-up"
+      signUpUrl="/sign-up"
       // // redirectUrl={redirectUrl || '/dashboard'}
       redirectUrl={"/challenge"}
       appearance={{
         elements: {
+          formFieldInput__identifier: "bg-input text-foreground",
+          formFieldInputShowPasswordButton: "stroke-foreground",
+          formFieldInputShowPasswordIcon: "stroke-foreground",
+          headerTitle: "text-card-foreground font-serif",
+          headerSubtitle: "text-card-foreground",
+          formFieldLabel: "text-card-foreground",
+          dividerLine: "bg-card-foreground",
+          dividerText: "text-card-foreground",
+          footerActionText: "text-card-foreground",
+          socialButtonsIconButton:
+            "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
+          card: "bg-card border-border",
           formButtonPrimary:
-            "bg-branding-green hover:bg-branding-green-800 tw-transition",
+            "bg-primary text-primary-foreground shadow hover:bg-primary/90",
           formFieldInput:
             "focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-branding-green",
           footerActionLink:
-            "text-branding-green font-bold hover:text-branding-green-800 tw-transition",
+            "text-primary underline-offset-4 hover:underline hover:text-primary",
         },
         layout: {
           //   logoImageUrl: 'https://clerk.dev/logo.png',
